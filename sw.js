a22717705e241a5402b3aa448fb58e748c2e53b7
rairaihtww <leidou865@gmail.com>
2026-08-07 21:38:25 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='football-now-v1';const FILES=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
